9f3edfd9f4c216dd1280d89606d6e854f886f0fb
Jeong-in <162551171+munginisfree@users.noreply.github.com>
2024-07-16 11:48:26 +0900

    # event, eventUser 연관관계 매핑

--- src/main/java/com/study/event/api/config/CrossOriginConfig.java
+++ src/main/java/com/study/event/api/config/CrossOriginConfig.java
@@ -7,14 +7,25 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 // 전역 크로스 오리진 설정 : 어떤 클라이언트를 허용할 것인지
 @Configuration
 public class CrossOriginConfig implements WebMvcConfigurer {
+
+    private String[] urls = {
+            "http://localhost:3000",
+            "http://localhost:3001",
+            "http://localhost:3002",
+            "http://localhost:3003",
+    };
+
     @Override
-    public void addCorsMappings(CorsRegistry registry){
+    public void addCorsMappings(CorsRegistry registry) {
+
         registry
-                .addMapping("/auth/**") // 어떤 url 요청에서
-                .allowedOrigins("http://localhost:3001") // 어떤 클라이언트를
+                .addMapping("/**") // 어떤 유알엘 요청에서
+                .allowedOrigins(urls) //어떤 클라이언트를
                 .allowedMethods("*") // 어떤 방식에서
                 .allowedHeaders("*") // 어떤 헤더를 허용할지
-                .allowCredentials(true); // 쿠키를 허용할지
+                .allowCredentials(true) // 쿠키 전송을 허용할지
         ;
+
     }
+
 }
